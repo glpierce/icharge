bf7b905e650e34aa05450e340a3b96cc62a2ee96
Grant Pierce <glpierce174@gmail.com>
2021-12-06 17:55:58 -0800

    Initial commit. Created index.html, index.js, style.css. Linked script and style sheets to index.html. Created basic address form in index.html. Created form submission event listener in index.js. Implemented basic API functionality in index.js for the PositionStack & OpenChargeMap APIs.

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const psaccesskey = '2924b2440dcf51f5ba91437412fead7d'
+const ocaccesskey = '9f2e41d5-3c41-43bc-9376-ad390fe352f4'
+let sourceLat
+let sourceLong
+
+document.addEventListener('DOMContentLoaded', event => {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', event => submitForm(event))
+})
+
+function submitForm(event) {
+    event.preventDefault()
+    const addressString = `${event.target[0].value}, ${event.target[1].value}, ${event.target[2].value} ${event.target[3].value} ${event.target[4].value}`
+    event.target.reset()
+    getCoordinates(addressString)
+}
+
+function getCoordinates(addressString) {
+    fetch(`http://api.positionstack.com/v1/forward?access_key=${psaccesskey}&query=${addressString}&limit=1`)
+    .then(resp => resp.json())
+    .then(coordData => {
+        sourceLat = parseFloat(coordData.data[0].latitude)
+        sourceLong = parseFloat(coordData.data[0].longitude)
+        getChargePoints()
+    })
+}
+
+function getChargePoints() {
+    const getObj = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    fetch(`https://api.openchargemap.io/v3/poi?latitude=${sourceLat}&longitude=${sourceLong}&distance=1.5&key=${ocaccesskey}`, getObj)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+    })
+}
